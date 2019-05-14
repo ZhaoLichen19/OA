0b35f4fa6eeb017bec23eab4953723af8f274061
ZhaoLichen19 <50104836+ZhaoLichen19@users.noreply.github.com>
2019-05-14 14:36:27 +0800

    Update index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,22 +1,88 @@
 <template>
   <div>
-    <my-leftlan></my-leftlan>
+    <div class="celan">
+      <my-leftlan></my-leftlan>
+    </div>
+    <my-header></my-header>
     <div class="container">
-      
+      <div class="shuju">
+        <div>
+          <i class="el-icon-goods"></i>
+          <span>上架商品总数</span>
+          <span v-text="goodsCount"></span>
+        </div>
+        <div>
+          <i class="el-icon-goods"></i>
+          <span>注册用户总数</span>
+          <span v-text="userCount"></span>
+        </div>
+        <div>
+          <i class="el-icon-goods"></i>
+          <span>已完成订单总数</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import myLeftlan from "@/components/leftlan.vue"
+import myLeftlan from "@/components/leftlan.vue";
+import myHeader from "@/components/header.vue";
 export default {
-  data(){
-    return{}
+  props: ["lid"],
+  data() {
+    return {
+      userCount: 0,
+      goodsCount: 0
+    };
   },
-  components:{
-      myLeftlan
+  created() {
+    this.getGoods();
+    this.getUser();
+  },
+  methods: {
+    getGoods() {
+      //创建变量保存请求地址
+      var url = "http://127.0.0.1:3000/getGoods";
+      //发送ajax请求，无参数
+      this.axios.get(url).then(result => {
+        this.goodsCount = result.data[0].c;
+      });
+    },
+    getUser() {
+      //创建变量保存请求地址
+      var url = "http://127.0.0.1:3000/getUser";
+      //发送ajax请求，无参数
+      this.axios.get(url).then(result => {
+        this.userCount = result.data[0].c;
+      });
     }
-}
+  },
+  components: {
+    myLeftlan,
+    myHeader
+  }
+};
 </script>
 <style scoped>
-
+.celan {
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 99;
+}
+.container {
+  margin-left: 215px;
+  height: 44.5rem;
+  display: flex;
+}
+.shuju {
+  display: flex;
+  width: 100%;
+}
+.shuju div {
+  width: 33.3%;
+}
+canvas {
+  background: #d76e8a;
+}
 </style>
